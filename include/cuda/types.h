1f1617d7e31367a6f9bbfd3e45ce4c2fdb6c566f
jwmccarthy <jack.mccarthy@autodesk.com>
2025-03-30 18:45:58 -0500

    more porting

--- include/cuda/types.h
+++ include/cuda/types.h
@@ -8,7 +8,8 @@ struct alignas(16) CudaVec {
 
     // Constructors
     __host__ __device__ CudaVec() : x(0), y(0), z(0), _w(0) {}
-    __host__ __device__ CudaVec(float x, float y, float z, float _w = 0) : x(x), y(y), z(z), _w(_w) {}
+    __host__ __device__ CudaVec(float x, float y, float z, float _w = 0) 
+        : x(x), y(y), z(z), _w(_w) {}
 
     // Basic operations
     __host__ __device__ CudaVec operator+(const CudaVec& other) const;
@@ -68,7 +69,8 @@ struct alignas(16) CudaRotMat {
     CudaVec f, r, u;
 
     __host__ __device__ CudaRotMat()
-    __host__ __device__ CudaRotMat(CudaVec f, CudaVec r, CudaVec u) : f(f), r(r), u(u) {}
+    __host__ __device__ CudaRotMat(CudaVec f, CudaVec r, CudaVec u) 
+        : f(f), r(r), u(u) {}
 
     __host__ __device__ static CudaRotMat GetIdentity();
     __host__ __device__ static CudaRotMat LookAt();
@@ -105,4 +107,32 @@ struct alignas(16) CudaRotMat {
     __host__ __device__ CudaVec Dot(const CudaVec& vec) const;
     __host__ __device__ CudaRotMat Dot(const CudaRotMat& other) const;
     __host__ __device__ CudaRotMat Transpose() const;
+};
+
+
+struct CudaAngle {
+    float yaw, pitch, roll;
+
+    __host__ __device__ CudaAngle(float yaw = 0, float pitch = 0, float roll = 0) 
+        : yaw(yaw), pitch(pitch), roll(roll) {}
+    
+    // Rotation matrix operations
+    __host__ __device__ static CudaAngle FromRotMat(CudaRotMat& mat);
+    __host__ __device__ CudaRotMat ToRotMat() const;
+
+    // Vector operations
+    __host__ __device__ static CudaAngle FromVec(const CudaVec& vec);
+    __host__ __device__ CudaVec GetForwardVec() const;
+
+    // Angle operations
+    __host__ __device__ void NormalizeFix();  // Constrain angles
+    __host__ __device__ CudaAngle GetDeltaTo(const CudaAngle& other) const;
+
+    // Basic operations
+    __host__ __device__ CudaAngle operator+(const CudaAngle& other) const;
+    __host__ __device__ CudaAngle operator-(const CudaAngle& other) const;
+
+    // Comparison operations
+    __host__ __device__ bool operator==(const CudaAngle& other) const;
+    __host__ __device__ bool operator!=(const CudaAngle& other) const;
 };
