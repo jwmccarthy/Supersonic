07eb80f20a1c0af379d58a3f9db1c66e614012d3
jwmccarthy <jwmccarthy11@gmail.com>
2025-05-11 00:51:37 -0500

    reverting last change. will wait until I work with CUDA constant memory to structure the default values like that

--- include/StateReset.cuh
+++ include/StateReset.cuh
@@ -8,23 +8,6 @@
 #include "RLConstants.cuh"
 #include "CudaCommon.cuh"
 
-struct CarResetDefaults {
-    float4 velocity        = make_float4(0.f, 0.f, 0.f, 0.f);
-    float4 angularVelocity = make_float4(0.f, 0.f, 0.f, 0.f);
-    float boostAmount      = SPAWN_BOOST_AMOUNT;
-    float demolishTimer    = 0.0f;
-    float demoCooldown     = 0.0f;
-    bool isOnGround        = true;
-    bool isSupersonic      = false;
-    bool hasJumped         = false;
-    bool hasDoubleJumped   = false;
-    bool isFlipping        = false;
-    bool isBoosting        = false;
-    bool isDemolished      = false;
-};
-
-__device__ const CarResetDefaults CAR_RESET_DEFAULTS;
-
 __device__ void resetBall(GameState* state, int simIdx);
 
 __device__ void resetCar(GameState* state, int carIdx, const CarSpawn loc, bool invert);
