80122bc50b2f9406bf4eaa181a48a5370a2392ee
jwmccarthy <jwmccarthy11@gmail.com>
2025-12-06 17:44:56 -0600

    Starting with face-face

--- src/CollisionsFace.cu
+++ src/CollisionsFace.cu
@@ -3,9 +3,30 @@
 #include "CollisionsFace.hpp"
 #include "CollisionsSAT.hpp"
 
+struct FaceVertices
+{
+    float4 p1;
+    float4 p2;
+    float4 p3;
+    float4 p4;
+};
+
 // Face-face collision manifold generation
 __device__ void generateFaceFaceManifold(SATContext& ctx, SATResult& res)
 {
-    // TODO: Implement Sutherland-Hodgman polygon clipping
-}
+    // Get reference face
 
+    // Get incident face (most negative dot product with reference normal)
+
+    // Get incident face vertices
+
+    // Project incident face vertices onto reference face plane
+
+    // Clip projected quad against reference face edges
+
+    // Transform clipped quad back to 3D space
+
+    // Cull points to best 4
+
+    // Output contact manifold
+}
